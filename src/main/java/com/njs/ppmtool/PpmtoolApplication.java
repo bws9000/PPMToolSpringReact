db6e1a6becac1cf7cd0b81f0e852e7b956b115e7
bws9000 <armorsoft@gmail.com>
2020-12-22 16:46:50 -0500

    first commit

---
+++ src/main/java/com/njs/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.njs.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
